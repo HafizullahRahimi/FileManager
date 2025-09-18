9ce73bc1eb93f3ad3311a3b0a18e631be2f44784
HafizullahRahimi <HafizzRahimi@outlook.com>
2025-09-18 18:58:18 +0200

    update profile image upload page

--- FileManager.Web/wwwroot/app.js
+++ FileManager.Web/wwwroot/app.js
@@ -1,4 +1,4 @@
-window.downloadFileFromStream = async (fileBytes, fileName) => {
+﻿window.downloadFileFromStream = async (fileBytes, fileName) => {
     const blob = new Blob([new Uint8Array(fileBytes)]);
     const url = window.URL.createObjectURL(blob);
     const a = document.createElement('a');
@@ -8,4 +8,14 @@ window.downloadFileFromStream = async (fileBytes, fileName) => {
     a.click();
     window.URL.revokeObjectURL(url);
     document.body.removeChild(a);
-}; 
+};
+
+getUrlFromInput = (inputId) => {
+    const input = document.getElementById(inputId);
+    const file = input.files[0];
+    if (!file) {
+        console.warn("⚠️ No file selected");
+        return null;
+    }
+    return URL.createObjectURL(file);
+};
